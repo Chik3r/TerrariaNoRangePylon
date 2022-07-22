048c8bde39a04fa958c96969b6b23c295e2de7aa
Chik3r <31808958+Chik3r@users.noreply.github.com>
2022-07-22 19:56:15 -0400

    More configs

--- NoRangePylon.cs
+++ NoRangePylon.cs
@@ -15,10 +15,24 @@ public class NoRangePylon : GlobalPylon {
 		return base.ValidTeleportCheck_PreNPCCount(pylonInfo, ref defaultNecessaryNPCCount);
 	}
 
+	public override bool? ValidTeleportCheck_PreAnyDanger(TeleportPylonInfo pylonInfo) {
+		if (ModContent.GetInstance<PylonConfig>().OverrideDangerLimit)
+			return true;
+		
+		return base.ValidTeleportCheck_PreAnyDanger(pylonInfo);
+	}
+
 	public override bool? ValidTeleportCheck_PreBiomeRequirements(TeleportPylonInfo pylonInfo, SceneMetrics sceneData) {
-		if (ModContent.GetInstance<PylonConfig>().OverrideTeleportAnywhere)
+		if (ModContent.GetInstance<PylonConfig>().OverrideBiomeLimit)
 			return true;
 		
 		return base.ValidTeleportCheck_PreBiomeRequirements(pylonInfo, sceneData);
+	}
+
+	public override bool? PreCanPlacePylon(int x, int y, int tileType, TeleportPylonType pylonType) {
+		if (ModContent.GetInstance<PylonConfig>().OverrideTypeLimit)
+			return true;
+		
+		return base.PreCanPlacePylon(x, y, tileType, pylonType);
 	}
 }
--- PylonConfig.cs
+++ PylonConfig.cs
@@ -26,5 +26,23 @@ namespace TerrariaNoRangePylon
 		[Range(0, 10)]
 		[DrawTicks]
 		public int RequiredNPCCount;
+		
+		[Header("Limit number of pylons")]
+		[Label("Disable the limit on the number of pylons")]
+		[Tooltip("Disables the limit that only allows one of each type of pylon.")]
+		[DefaultValue(false)]
+		public bool OverrideTypeLimit;
+		
+		[Header("Biome limit")]
+		[Label("Disable the biome check")]
+		[Tooltip("This will allow you to teleport to pylons that are placed in any biome.")]
+		[DefaultValue(false)]
+		public bool OverrideBiomeLimit;
+		
+		[Header("Danger limit")]
+		[Label("Disable the danger checks")]
+		[Tooltip("Allows you to teleport to pylons while ignoring their danger checks.")]
+		[DefaultValue(false)]
+		public bool OverrideDangerLimit;
 	}
 }
